3e827dcac8b580bf1ca9a113d4d92356a75ca9c4
RJ4RJ <135825245+RJ4RJ@users.noreply.github.com>
2024-05-01 21:42:56 +0530

    Update AddTicket.java

--- Main code/src/main/java/com/event/AddTicket.java
+++ Main code/src/main/java/com/event/AddTicket.java
@@ -14,8 +14,7 @@ public class AddTicket extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		
+			
 		
 	}
 
